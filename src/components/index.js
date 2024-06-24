a955b404b7b5f068062c7a2f4d7ca8488c63dea9
Saurabh-Taylor <tailorsaurabh12@gmail.com>
2024-06-24 14:21:51 +0530

    added react-hook-form and implemented its functionality

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,20 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 import Logo from "./Logo";
 import Container from "./container/Container";
+
+import Button from "./Button";
+
 export {
     Footer,
     Header,
     LogoutBtn,
     Logo,
-    Container
+    Container,
+    Input,
+    Button
 }
 
 
